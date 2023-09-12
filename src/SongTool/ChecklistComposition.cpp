14de12d4a248a7ed984ead51de5cf21506257371
Seppo Pakonen <seppo.pakonen@gmail.com>
2023-09-12 20:49:12 +0300

    composition checklist tab

--- src/SongTool/ChecklistComposition.cpp
+++ src/SongTool/ChecklistComposition.cpp
@@ -2,9 +2,68 @@
 
 
 ChecklistComposition::ChecklistComposition() {
+	Add(list.SizePos());
 	
+	list.AddColumn("Key");
+	list.AddColumn("Ready?");
+	list.AddColumn("Comments");
+	list.ColumnWidths("6 1 24");
+	
+	#define CHK_COMP(e, s) list.Add(s); keys.Add(#e);
+	CHK_COMPOSITION_LIST
+	#undef CHK_COMP
+	for(int i = 0; i < list.GetCount(); i++) {
+		Option* o = new Option;
+		opts.Add(o);
+		o->WhenAction << THISBACK1(OnOptionChange, i);
+		list.SetCtrl(i, 1, o);
+		
+		EditString* e = new EditString;
+		edits << e;
+		e->WhenAction << THISBACK1(OnValueChange, i);
+		list.SetCtrl(i, 2, e);
+	}
 }
 
 void ChecklistComposition::Data() {
+	Database& db = Database::Single();
+	EditorPtrs& p = db.ctx.ed;
+	if (!p.song) return;
+	Song& song = *p.song;
 	
+	for(int i = 0; i < list.GetCount(); i++) {
+		int b = StrInt(song.data.Get(keys[i] + "_VALUE", "0"));
+		opts[i]->Set(b);
+		String comment = song.data.Get(keys[i] + "_COMMENT", "");
+		list.Set(i, 2, comment);
+	}
+	
+}
+
+void ChecklistComposition::OnValueChange(int i) {
+	Database& db = Database::Single();
+	EditorPtrs& p = db.ctx.ed;
+	if (!p.song) return;
+	Song& song = *p.song;
+	
+	String key = keys[i] + "_COMMENT";
+	String value = list.Get(i, 2);
+	int j = song.data.Find(key);
+	if (j < 0 && value.IsEmpty())
+		return;
+	song.data.GetAdd(key) = value;
+}
+
+void ChecklistComposition::OnOptionChange(int i) {
+	Database& db = Database::Single();
+	EditorPtrs& p = db.ctx.ed;
+	if (!p.song) return;
+	Song& song = *p.song;
+	
+	Option* o = dynamic_cast<Option*>(list.GetCtrl(i, 1));
+	if (o) {
+		int b = o->Get();
+		String key = keys[i] + "_VALUE";
+		song.data.GetAdd(key) = IntStr(b);
+	}
 }
--- src/SongTool/ChecklistComposition.h
+++ src/SongTool/ChecklistComposition.h
@@ -2,14 +2,40 @@
 #define _SongTool_ChecklistComposition_h_
 
 
+#define CHK_COMPOSITION_LIST \
+	CHK_COMP(CHORD_PAD, "chord pad") \
+	CHK_COMP(VOCAL_MELODY, "vocal melody") \
+	CHK_COMP(MID_SOLO, "mid solo (instrumental theme)") \
+	CHK_COMP(HIGH_SOLO, "high solo (e.g. bridge climax solo)") \
+	CHK_COMP(MID_SOLO_FILLS, "mid solo fills (e.g. between singing)") \
+	CHK_COMP(HIGH_RHYTHM, "high rhythm (e.g. high muted guitar)") \
+	CHK_COMP(MID_RHYTHM, "mid rhythm (low guitar, e.g. strumming)") \
+	CHK_COMP(LOW_RHYTHM, "low rhythm (bass)") \
+	CHK_COMP(LOW_SOLO, "low solo (bass solo)") \
+	CHK_COMP(IMPACTS, "impacts") \
+	CHK_COMP(TEXTURE_PAD, "texture pad (arp)")
+
+
 class ChecklistComposition : public SongToolCtrl {
+	ArrayCtrl list;
+	Vector<String> keys;
+	Vector<Option*> opts;
+	Vector<EditString*> edits;
 	
+	enum {
+		#define CHK_COMP(e, s) e,
+		CHK_COMPOSITION_LIST
+		#undef CHK_COMP
+		ITEM_LIST
+	};
 	
 public:
 	typedef ChecklistComposition CLASSNAME;
 	ChecklistComposition();
 	
 	void Data();
+	void OnOptionChange(int i);
+	void OnValueChange(int i);
 	
 };
 
