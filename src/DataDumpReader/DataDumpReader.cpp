ddf0a57331570c2d14a6cdc5477be0ee21e1ddb0
Seppo Pakonen <seppo.pakonen@gmail.com>
2024-08-17 00:43:49 +0000

    DataDumpReader reads essential twitter data dump fields

--- src/DataDumpReader/DataDumpReader.cpp
+++ src/DataDumpReader/DataDumpReader.cpp
@@ -15,11 +15,71 @@ CONSOLE_APP_MAIN
 }
 
 void DoFn(int fn) {
-	// Twitter
+	
 	if (fn == 0) DoTwitter();
+	else {LOG("error: invalid function arg");}
 	
 }
 
 void DoTwitter() {
+	String dir = GetHomeDirFile("twitter-data");
+	if (!DirectoryExists(dir)) {
+		LOG("error: no directory: " << dir);
+		return;
+	}
 	
+	String tweets_file = AppendFileName(dir, "data" DIR_SEPS "tweets.js");
+	String like_file = AppendFileName(dir, "data" DIR_SEPS "like.js");
+	
+	if (!FileExists(tweets_file)) {
+		LOG("error: no file: " << tweets_file);
+		return;
+	}
+	
+	if (!FileExists(like_file)) {
+		LOG("error: no file: " << tweets_file);
+		return;
+	}
+	
+	{
+		String content = LoadFile(like_file);
+		Vector<String> lines = Split(content,"\n");
+		if (lines.GetCount() < 2) {LOG("error: unexpected file content"); return;}
+		lines[0] = "[";
+		content = Join(lines, "\n");
+		
+		ValueArray arr = ParseJSON(content);
+		
+		for(int i = 0; i < arr.GetCount(); i++) {
+			ValueMap like = arr.At(i).GetAdd("like");
+			String id = like.GetAdd("tweetId");
+			String full_text = like.GetAdd("fullText");
+			LOG(i << ": " << id << ": " << full_text);
+		}
+	}
+	
+	{
+		String content = LoadFile(tweets_file);
+		Vector<String> lines = Split(content,"\n");
+		if (lines.GetCount() < 2) {LOG("error: unexpected file content"); return;}
+		lines[0] = "[";
+		content = Join(lines, "\n");
+		
+		ValueArray arr = ParseJSON(content);
+		
+		
+		for(int i = 0; i < arr.GetCount(); i++) {
+			ValueMap tweet = arr.At(i).GetAdd("tweet");
+			
+			Value retweeted = tweet.GetAdd("retweeted");
+			bool is_retweeted = (int)retweeted;
+			String lang = tweet.GetAdd("lang");
+			String full_text = tweet.GetAdd("full_text");
+			String in_reply_to_status_id = tweet.GetAdd("in_reply_to_status_id");
+			bool is_in_reply = in_reply_to_status_id.GetCount();
+			LOG(i << ": " << lang << ": " << full_text);
+		}
+	}
+	
+	LOG("Finish!");
 }
